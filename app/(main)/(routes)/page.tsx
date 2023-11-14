653325f577fe1f2a9e55cf4fd1e479d201085439
Valerian <vtouzard244@gmail.com>
2023-11-14 16:49:58 +0100

    feat: Ajout du mode dark / light

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <UserButton afterSignOutUrl='/'/>
+    <div>
+      <UserButton afterSignOutUrl='/'/>
+      <ModeToggle />
+    </div>
   )
 }
