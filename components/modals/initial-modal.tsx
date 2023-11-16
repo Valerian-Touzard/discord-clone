a02040b544af481fbe89440c2dc5e00f5a1a04be
Valerian <vtouzard244@gmail.com>
2023-11-16 17:15:21 +0100

    feat: ajout de la création d'un server sur prisma studio

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
@@ -26,6 +27,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {message: "Le nom du serveur est obligatoire"}),
@@ -36,6 +38,8 @@ const formSchema = z.object({
 export const InitialModal = () =>{
 
     const [isMounted, setIsMounted] = useState(false);
+
+    const router = useRouter()
 
     useEffect(() =>{
         setIsMounted(true)
@@ -52,7 +56,15 @@ export const InitialModal = () =>{
     const isLoading = form.formState.isSubmitted;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values);
+
+            form.reset();
+            router.refresh();
+            window.location.reload()
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if(!isMounted){
