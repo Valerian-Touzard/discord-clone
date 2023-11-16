d707d29192d1a29b59cf0e5cef773ef921c7bfc3
Valerian <vtouzard244@gmail.com>
2023-11-16 15:55:35 +0100

    fix: maj sdk upload thing + ajout d'une configuration

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -80,11 +80,11 @@ export const InitialModal = () =>{
                                     render={({ field }) => (
                                         <FormItem>
                                             <FormControl>
-                                                <FileUpload 
+                                            <FileUpload 
                                                     endpoint="serverImage"
                                                     value={field.value}
                                                     onChange={field.onChange}
-                                                />
+                                            />
                                             </FormControl>
                                         </FormItem>
                                     )}
